a6efcb9576fbc3fc5dd268c2b02208941a4012e3
Flopochka <ym.uvtqk@gmail.com>
2025-03-08 09:49:07 +0300

    Перехожу на вёрстку шаблонами

--- src/components/MainStars.vue
+++ src/components/MainStars.vue
@@ -7,25 +7,25 @@ defineProps({
 </script>
 
 <template>
-    <main>
-        <div class="select-type">
-            <div class="select-type-item select-type-item-selected">
+    <main class="flex-col gap-28 p-24">
+        <div class="select-type flex-row gap-4 bg-blue-900 rounded-10 p-2">
+            <div class="select-type-item select-type-item-selected flex-row items-center justify-center gap-4 text-white p-6 rounded-8 bg-blue-500">
                 Stars
-                <img src="../assets/3456345345345345.png" alt="" class="select-type-item-img">
+                <img src="../assets/3456345345345345.png" alt="" class="img-16">
             </div>
-            <div class="select-type-item">
+            <div class="select-type-item flex-row items-center justify-center gap-4 text-white p-6 rounded-8 bg-blue-500">
                 Premium
-                <img src="../assets/23765848672354867236548723.png" alt="" class="select-type-item-img">
+                <img src="../assets/23765848672354867236548723.png" alt="" class="img-16">
             </div>
         </div>
-        <div class="select-top select-top-1">
-            <div class="select-top-item">
-                <p class="select-top-item-text">Username</p>
-                <input type="text" class="select-top-item-input-text" placeholder="@example">
+        <div  class="select-top select-top-1 flex-col gap-16">
+            <div class="select-top-item flex-col gap-6">
+                <p class="pl-12">Username</p>
+                <input type="text" class="select-top-item-input-text rounded-12 bg-neutral-200 text-neutral-700 text-14" placeholder="@example">
             </div>
-            <div class="select-top-item">
-                <p class="select-top-item-text">Username</p>
-                <input type="text" class="select-top-item-input-text" placeholder="@example">
+            <div class="select-top-item flex-col gap-6">
+                <p class="pl-12">Username</p>
+                <input type="text" class="select-top-item-input-text rounded-12 bg-neutral-200 text-neutral-700 text-14" placeholder="@example">
             </div>
             <div class="select-top-item select-top-stars-box">
                 <div class="select-top-stars-card">
@@ -43,34 +43,34 @@ defineProps({
             </div>
         </div>
         <div class="select-bottom">
-            <p class="select-bottom-text">Payment</p>
-            <div class="select-botoom-cards">
-                <div class="select-bottom-card select-bottom-card-active">
-                    <div class="select-bottom-card-custom-radio select-bottom-card-custom-radio-active"></div>
-                    <p class="select-bottom-card-text">TON Network</p>
-                    <img src="" alt="" class="select-bottom-card-img">
+            <p class="pl-12">Payment</p>
+            <div class="select-botoom-cards grid-row gap-8">
+                <div class="select-bottom-card select-bottom-card-active card bg-blue-900 grid-col items-center gap-8">
+                    <div class="custom-radio custom-radio-active"></div>
+                    <p class="text-16 text-contrast">TON Network</p>
+                    <img src="" alt="" class="img-16">
                 </div>
-                <div class="select-bottom-card">
-                    <div class="select-bottom-card-custom-radio"></div>
-                    <p class="select-bottom-card-text">USDT</p>
-                    <img src="" alt="" class="select-bottom-card-img">
+                <div class="select-bottom-card card bg-blue-900 grid-col items-center gap-8">
+                    <div class="custom-radio"></div>
+                    <p class="text-16 text-contrast">USDT</p>
+                    <img src="" alt="" class="img-16">
                 </div>
-                <div class="select-bottom-card">
-                    <div class="select-bottom-card-custom-radio"></div>
-                    <p class="select-bottom-card-text">SBP</p>
-                    <img src="" alt="" class="select-bottom-card-img">
+                <div class="select-bottom-card card bg-blue-900 grid-col items-center gap-8">
+                    <div class="custom-radio"></div>
+                    <p class="text-16 text-contrast">SBP</p>
+                    <img src="" alt="" class="img-16">
                 </div>
-                <div class="select-bottom-card">
-                    <div class="select-bottom-card-custom-radio"></div>
-                    <p class="select-bottom-card-text">VISA & MasterCard</p>
-                    <img src="" alt="" class="select-bottom-card-img">
+                <div class="select-bottom-card card bg-blue-900 grid-col items-center gap-8">
+                    <div class="custom-radio"></div>
+                    <p class="text-16 text-contrast">VISA & MasterCard</p>
+                    <img src="" alt="" class="img-16">
                 </div>
             </div>
         </div>
-        <div class="bottom-button">
-            <img src="../assets/3456345345345345.png" alt="" class="bottom-button-img">
-            <p class="bottom-button-text">Buy 3 500</p>
-            <img src="../assets/3456345345345345.png" alt="" class="bottom-button-img">
+        <div class="bottom-button btn bg-gradient-blue">
+            <img src="../assets/3456345345345345.png" alt=""  class="img-16">
+            <p class="text-17 font-geist font-600 letter-spacing-04 text-white">Buy 3 500</p>
+            <img src="../assets/3456345345345345.png" alt=""  class="img-16">
         </div>
     </main>
 </template>
@@ -80,29 +80,17 @@ main{
     box-sizing: border-box;
     min-height: calc(100vh - 100px);
     width: 100%;
-    padding: 24px;
-    gap: 28px;
     display: grid;
     grid-template-rows: repeat(3, auto) 1fr;
     align-items: end;
 }
 .select-type {
     width: 100%;
-    display: flex;
-    gap: 4px;
-    background: var(--blue-900);
-    border-radius: 10px;
     padding: 2px;
 }
 .select-type-item {
     width: 100%;
-    color: var(--white-100);
     padding: 6px;
-    border-radius: 8px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 4px;
 }
 .select-type-item-selected {
     background: var(--blue-500);
@@ -112,27 +100,15 @@ main{
 }
 .select-top {
     width: 100%;
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
 }
 .select-top-1 {
 }
 .select-top-item {
-    display: flex;
-    flex-direction: column;
     gap: 6px;
 }
-.select-top-item-text{
-    padding-left: 12px;
-}
 .select-top-item-input-text {
-    border-radius: 12px;
     padding: 15px 12px;
-    background: var(--neutral-200);
-    color: var(--neutral-700);
     border: 0;
-    font-size: 14px;
 }
 .select-top-stars-box {
     display: grid;
@@ -157,78 +133,21 @@ main{
 }
 .select-bottom {
 }
-.select-bottom-text {
-    padding-left: 12px;
-}
 .select-botoom-cards {
-    display: grid;
     grid-template-rows: repeat(4, 1fr);
-    gap: 8px;
-
 }
 .select-bottom-card {
     width: 100%;
-    background: var(--blue-900);
-    border-radius: 10px;
-    padding: 12px;
-    display: grid;
-    align-items: center;
     grid-template-columns: auto auto 1fr;
-    gap: 8px;
 }
 .select-bottom-card-active {
     border: 2px solid var(--System-azure-700-80, #007AFFCC)
 }
-.select-bottom-card-custom-radio {
-    width: 18px;
-    height: 18px;
-    border: 2px solid var(--neutral-300);
-    border-radius: 50%;
-}
-.select-bottom-card-active .select-bottom-card-custom-radio{
-    border: 2px solid var(--System-azure-700, var(--blue-700-80));
-    position: relative;
-}
-.select-bottom-card-active .select-bottom-card-custom-radio::after{
-    content: "";
-    display: block;
-    width: 9px;
-    height: 9px;
-    border-radius: 50%;
-    background: var(--blue-700-80);
-    position: absolute;
-    left: calc(50% - 4.5px);
-    top: calc(50% - 4.5px);
-}
-.select-bottom-card-text {
-    font-size: 16px;
-}
-.select-bottom-card-active .select-bottom-card-text{
-    color: var(--contrast);
-}
-.select-bottom-card-img {
-}
 .bottom-button {
     width: 100%;
     background: linear-gradient(129.45deg, var(--blue-400) 9.38%, var(--blue-600) 117.65%);
-    width: 354;
-    min-width: 50px;
-    padding: 15px 12px;
-    gap: 4px;
-    border-radius: 12px;
-    display: flex;
-    gap: 4px;
-    align-items: center;
-    justify-content: center;
 }
 .bottom-button-img {
     height: 16px;
-}
-.bottom-button-text {
-    font-size: 17px;
-    font-family: Geist;
-    font-weight: 600;
-    letter-spacing: -0.4px;
-    color: var(--white-100);
 }
 </style>
