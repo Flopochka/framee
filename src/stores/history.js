68d0b32e2a39039f0fb950598858926006a109d8
Flopochka <ym.uvtqk@gmail.com>
2025-04-27 19:37:44 +0300

    df

---
+++ src/stores/history.js
@@ -0,0 +1,11 @@
+// src/stores/userStore.js
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useHistoryStore = defineStore('history', () => {
+  
+
+  return {
+    
+  };
+});
