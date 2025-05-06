e68afed6fe1610f1c1588eff9a174398bc0b14d5
Flopochka <ym.uvtqk@gmail.com>
2025-05-06 23:28:33 +0300

    dgf

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 
-init();
+init().then(({ webApp }) => {
+  const userId = webApp.initDataUnsafe?.user?.id;
+  console.log("User ID:", userId);
+});
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
