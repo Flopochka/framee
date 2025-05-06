7754083e89c7d5e9891721ed1548a90775322c69
Flopochka <ym.uvtqk@gmail.com>
2025-05-06 23:40:01 +0300

    ывапвп

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+const tg = window.Telegram.WebApp;
+tg.ready(); // уведомляем Telegram, что WebApp готов
+tg.expand(); // разворачиваем на весь экран
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.mount("#app");
-window.Telegram.WebApp.expand();
