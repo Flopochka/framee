a7d0bfc2f65f9482404c6574b4d35e6469541fbd
Flopochka <ym.uvtqk@gmail.com>
2025-05-06 23:35:43 +0300

    варп

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { init } from "@telegram-apps/sdk-vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-
-init().then(({ webApp }) => {
-  const userId = webApp.initDataUnsafe?.user?.id;
-  console.log("User ID:", userId);
-});
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.mount("#app");
+window.Telegram.WebApp.expand();
