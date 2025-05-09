ae0578a2f25216e159f374f5a92dfbd11f90f967
Flopochka <ym.uvtqk@gmail.com>
2025-05-09 22:15:23 +0300

    sdf

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from "./App.vue";
 import router from "./router";
 import WebApp from "@twa-dev/sdk";
 WebApp.ready();
+if (!window.Telegram?.WebApp?.initData) {window.location = "https://t.me/Framestars_bot"}
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
