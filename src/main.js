8ebf081bb56be879509912fa28df390c563d0240
Flopochka <ym.uvtqk@gmail.com>
2025-05-09 20:28:57 +0300

    sf

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from "./App.vue";
 import router from "./router";
 import WebApp from "@twa-dev/sdk";
 WebApp.ready();
+console.log(WebApp.isExpanded)
 WebApp.expand();
 const app = createApp(App);
 app.use(createPinia());
