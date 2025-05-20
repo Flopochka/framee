900fe701bb5e68b018a685205bdb3c6de57b0d12
Flopochka <ym.uvtqk@gmail.com>
2025-05-20 21:00:33 +0300

    idk

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,7 @@ onMounted(() => {
     token:
       "eyJhcHBfbmFtZSI6ImZyYW1lZGF0YSIsImFwcF91cmwiOiJodHRwczovL3QubWUvRnJhbWVzdGFyc19ib3QiLCJhcHBfZG9tYWluIjoiaHR0cHM6Ly9mcmFtZS1zdGFycy5jb20ifQ==!PFVXgcAK8T2AqIn0NfxyTwY3ualDHJeY/9y7clsmzto=",
     appName: "FRAME",
+    userId: 227363776,
   });
 
   // Инициализация lazy-loading
