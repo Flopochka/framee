f52bb4ccecd84c900ca6a2744438d27d11fbd779
Flopochka <ym.uvtqk@gmail.com>
2025-05-24 19:45:15 +0300

    g

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -77,7 +77,7 @@ async function copyToClipboard(text) {
 function shareContent() {
   const shareUrl = `https://t.me/share/url?url=${encodeURIComponent(
     " "
-  )}&text=${encodeURIComponent(shareData.text + " \n\т" + shareData.url+" ")}`;
+  )}&text=${encodeURIComponent(shareData.text + " \n\n" + shareData.url+" ")}`;
   if (window.Telegram?.WebApp?.openTelegramLink) {
     window.Telegram.WebApp.openTelegramLink(shareUrl);
   } else {
