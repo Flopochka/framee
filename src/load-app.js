358a672c9923587b3cef532da366a71a78870708
Flopochka <ym.uvtqk@gmail.com>
2025-05-27 17:53:26 +0300

    f

--- src/load-app.js
+++ src/load-app.js
@@ -18,19 +18,14 @@ function isMobileDevice() {
 // Если это не мобильное устройство, выходим из полноэкранного режима
 if (!isMobileDevice()) {
   console.log("[load-app] не мобильное устройство");
-  WebApp
-    .exitFullscreen()
-    .then(() => {
-      console.log("[load-app] Выход из полноэкранного режима.");
-    })
-    .catch((error) => {
-      console.error(
-        "[load-app] Не удалось выйти из полноэкранного режима:",
-        error
-      );
-    });
-}else{
-    console.log("[load-app] мобильное устройство");
+  WebApp.exitFullscreen().catch((error) => {
+    console.error(
+      "[load-app] Не удалось выйти из полноэкранного режима:",
+      error
+    );
+  });
+} else {
+  console.log("[load-app] мобильное устройство");
 }
 
 WebApp.ready();
