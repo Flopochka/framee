fe70c21ea579c5ac8bd3ff28c1c7e1bad7ceee6c
Flopochka <ym.uvtqk@gmail.com>
2025-07-15 19:37:38 +0300

    refactor: improve code formatting and consistency in screen.js

--- src/stores/screen.js
+++ src/stores/screen.js
@@ -1,30 +1,31 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
-export const useScreenStore = defineStore('screen', () => {
-  const currentScreen = ref(0)
-  const router = useRouter()
-  const routes = ['/', '/tasks', '/about', '/profile', '/withdraw']
+export const useScreenStore = defineStore("screen", () => {
+  const currentScreen = ref(0);
+  const router = useRouter();
+  const routes = ["/", "/tasks", "/about", "/profile", "/withdraw"];
   const switchScreen = async (type) => {
-    currentScreen.value = type
-    router.push(routes[type])
-  }
-  const getCurrentScreen = () => currentScreen.value
+    currentScreen.value = type;
+    router.push(routes[type]);
+  };
+  const getCurrentScreen = () => currentScreen.value;
   const syncWithRoute = (path) => {
     const screenMap = {
-      '/': 0,
-      '/tasks': 1,
-      '/about': 2,
-      '/profile': 3,
-      '/withdraw': 4
-    }
-    currentScreen.value = screenMap[path] ?? 0
-  }
+      "/": 0,
+      "/tasks": 1,
+      "/about": 2,
+      "/profile": 3,
+      "/withdraw": 4,
+    };
+    currentScreen.value = screenMap[path] ?? 0;
+    router.push(path);
+  };
 
   return {
     switchScreen,
     getCurrentScreen,
-    syncWithRoute
-  }
-})
+    syncWithRoute,
+  };
+});
